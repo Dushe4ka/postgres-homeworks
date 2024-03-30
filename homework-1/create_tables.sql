e8c81fc824d673fdcacb767e4e65d765a2ad15bf
Pavel Golubinec <7respawnables7@gmail.com>
2024-03-30 22:08:32 +0300

    30.03.2024

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+create table customers(
+customer_id varchar(20) primary key NOT null Unique,
+company_name varchar(20),
+contact_name varchar(20)
+);
+select * from customers;
+
+create table employees(
+employee_id serial primary key,
+first_name varchar(20),
+last_name varchar(20),
+title varchar(20),
+birth_date date,
+notes text
+);
+
+select * from employees;
+
+create table orders(
+order_id int,
+customer_id varchar(20) references customers(customer_id),
+employee_id int references employees(employee_id),
+order_date date,
+ship_city varchar(20)
+);
+
+select * from orders;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,57 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+def opening_customers():
+    with open('north_data/customers_data.csv', newline='') as f:
+        list_cvs = []
+        filereader = csv.reader(f, quotechar='|')
+        next(filereader)
+        for elem in filereader:
+            list_cvs.append(elem)
+        return list_cvs
+
+
+def opening_employees():
+    with open('north_data/employees_data.csv', newline='') as f:
+        list_csv = []
+        filereader = csv.reader(f, quotechar='|')
+        next(filereader)
+        for elem in filereader:
+            list_csv.append(elem)
+        return list_csv
+
+
+def opening_orders():
+    with open('north_data/orders_data.csv', newline='') as f:
+        list_csv_order = []
+        filereader = csv.reader(f, quotechar='|')
+        for elem in filereader:
+            next(filereader)
+            list_csv_order.append(elem)
+        return list_csv_order
+
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="12357985"
+)
+
+cur = conn.cursor()
+# rows = opening_customers()
+#
+#
+# cur.executemany("INSERT INTO customers VALUES (%s, %s, %s)", rows)
+# cur.execute('SELECT * FROM customers')
+row_emp = opening_employees()
+for i in row_emp:
+    print(i)
+cur.executemany("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", row_emp)
+# row_order = opening_orders()
+# cur.executemany("INSERT INTO customers VALUES (%s, %s, %s)", row_order)
+conn.commit()
+
+
+
